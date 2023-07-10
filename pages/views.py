66bcb73d2b4d2bd03f30d046cf12961f8a7e0145
yego21 <jakevillamor222@gmail.com>
2023-07-10 15:10:02 +0800

    ff

--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,17 @@ from django.shortcuts import get_object_or_404, render
 
 def home(request):
     return render(request, 'pages/home.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def cars(request):
+    return render(request, 'pages/cars.html')
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
         
         
