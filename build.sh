ee4a7777e1b15d01706012ae88760704138f7829
yego21 <jakevillamor222@gmail.com>
2023-08-05 02:40:32 +0800

    login

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ pip install --upgrade pip
 pip install -r build.sh
 pip install -r requirements.txt
 python manage.py collectstatic
-#python manage.py loaddata project_dump.json
-#python manage.py migrate
+python manage.py loaddata project_dump.json
+python manage.py migrate
